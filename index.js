17e973428fbe8fe3000a358883fdf2f9481d60b8
seasons986 <annalin12345@gmail.com>
2023-09-20 00:40:58 -0400

    added an end

--- index.js
+++ index.js
@@ -13,25 +13,32 @@ window.addEventListener("load", () => {
 
   function getItem() {
     const gm = Object.keys(groceries);
+    let found = [];
+    let count = gm.length;
     let itemIndex = getRandomInt(0, gm.length);
-    while (total < groceries[gm[itemIndex]]) {
+    while (total < groceries[gm[itemIndex]] && count > 0 && !found.includes(itemIndex)) {
       itemIndex = getRandomInt(0, gm.length);
+      found.push(itemIndex);
+      count--;
     }
-    return groceries[gm[itemIndex]];
+    return count == 0 ? null : groceries[gm[itemIndex]];
   }
 
   function displayItem(init = false) {
     const item = getItem();
+    let end = false;
+    if (!item) end = true;
     const dialog = document.createElement("dialog");
     const p = document.createElement("p");
     const form = document.createElement("form");
     const button = document.createElement("button");
-    if (!init) p.innerHTML += `you spent $${item.regularPrice} on ${item.name}<br><br>`;
-    p.innerHTML += `you have $${total} to spend`;
+    if (end) p.innerHTML += `there's nothing else you can buy`;
+    else if (!init) p.innerHTML += `you spent $${item.regularPrice} on ${item.name}<br><br>`;
+    p.innerHTML += `you have $${total} ${end ? ", good luck." : "to spend"}`;
     dialog.appendChild(p);
     if (!init) button.innerHTML = "OK";
     else button.innerHTML = "let's shop";
-    form.appendChild(button);
+    if (!end) form.appendChild(button);
     dialog.appendChild(form);
     dialog.open = true;
     button.onclick = (e) => {
@@ -39,7 +46,7 @@ window.addEventListener("load", () => {
       displayItem();
     }
     if (!init) { dialog.style.left = `${getRandomInt(0,w)}px`; dialog.style.top = `${getRandomInt(0,h)}px` }
-    else { dialog.style.left = "5%"; dialog.style.top = "5%"; }
+    else { dialog.style.left = "15%"; dialog.style.top = "15%"; }
     container.appendChild(dialog);
     total = Math.round((total - item.regularPrice) * 100) / 100;
   }
